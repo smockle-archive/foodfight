c6017ac6a725d2dcf839535f2a700bfbaf6628c3
Jake <javakatdesign@gmail.com>
2014-02-12 22:52:26 -0600

    Add unity project, basic units
    Can now generate units in Unity Editor with differing healths. Units can
    be tagged as on the player's team or the enemy's and will be colored to
    match this fact. Units can be clicked to deal damage to their adjacent
    enemy units.

---
+++ foodfight/Assets/Scripts/TeamColorScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TeamColorScript : MonoBehaviour {
+
+    Color color;
+
+	// Use this for initialization
+	void Start () {
+
+        foreach (var unitScript in GameObject.FindObjectsOfType<UnitScript>())
+        {
+            switch (unitScript.gameObject.tag)
+            {
+                case "Unit - Player":
+                    color.r = .5f;
+                    color.g = .5f;
+                    color.b = 1;
+                    color.a = 1;
+                    break;
+                case "Unit - Enemy":
+                    color.r = 1;
+                    color.g = .5f;
+                    color.b = .5f;
+                    color.a = 1;
+                    break;
+                default:
+                    Debug.Log("ERROR: Object <" + unitScript.gameObject.name + "> has no affiliated team.");
+                    color = Color.gray;
+                    break;
+            }
+            unitScript.gameObject.renderer.material.color = color;
+        }
+	}
+	
+}
---
+++ foodfight/Assets/Scripts/UnitScript.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnitScript : MonoBehaviour {
+
+    // maybe change these back to standard scope instead of public? 
+    // don't probably need to see these attributes once the game works...
+    public int health;
+    public int attack;
+    public int move;
+    public int x;
+    public int y;
+
+    public UnitScript defender;
+
+    bool canMove = true;
+
+    void Attack (UnitScript defender)
+    {
+        if(canAttack(defender)) defender.health -= this.attack;
+    }
+    void Move (int x, int y)
+    {
+        if (canMove && Mathf.Abs(this.x - x) + Mathf.Abs(this.y - y) <= move)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    bool canAttack(UnitScript defender)
+    {
+        //TODO: FILL THIS IN
+        return true; 
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            float x = Input.mousePosition.x;
+            float y = Input.mousePosition.y;
+
+            //Debug.Log("We're at least firing this damn thing! x: " + x + ", y: " + y);
+            //Debug.Log(this.name + " is at x: " + Camera.main.WorldToScreenPoint(this.transform.position).x + ", y: " + Camera.main.WorldToScreenPoint(this.transform.position).y);
+
+            if (
+                (Camera.main.WorldToScreenPoint(this.transform.position).x <= x) && (Camera.main.WorldToScreenPoint(this.transform.position).x + 200 >= x) //we're within the x-bounds of the unit
+                &&
+                (Camera.main.WorldToScreenPoint(this.transform.position).y <= y) && (Camera.main.WorldToScreenPoint(this.transform.position).y + 200 >= y) //we're within the y-bounds of the unit
+            )
+            {
+                this.Attack(this.defender);
+                Debug.Log(this.defender.name + " took " + this.attack + " damage! Its health is now " + this.defender.health + ".");
+            }
+        }
+    }
+}
